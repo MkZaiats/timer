df58ec90477dc27a1db536c34f681ed498615aa2
tuytuy3 <89216147+tuytuy3@users.noreply.github.com>
2021-12-06 20:38:20 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+let date = new Date ()
+
+const hours = document.querySelector('.hours')
+const minutes = document.querySelector('.minutes')
+const month = document.querySelector('.month')
+
+const months = ["January","February","March","April","May",
+"June","July","August","September","October","November","December"]
+
+hours.textContent = date.getHours() - 1
+minutes.textContent = date.getMinutes()
+month.textContent = `${date.getDate()} ${months[date.getMonth()]}`
+let seconds = date.getSeconds()
+console.log(seconds);
+
+function timer () {
+    setInterval(() => {
+        date = new Date ()
+        seconds = date.getSeconds()
+        if (seconds === 1) {
+            hours.textContent = date.getHours() - 1
+            minutes.textContent = date.getMinutes()
+            month.textContent = `${date.getDate()} ${months[date.getMonth()]}`
+        }
+    },1000)
+}
+
+timer()
